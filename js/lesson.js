6bdd04f100f54cc3029a2d36e218614805ada2e1
Stariy-131 <antonkuzmin2006men@gmail.com>
2026-02-16 02:54:14 +0600

    Created a card switcher

--- js/lesson.js
+++ js/lesson.js
@@ -26,7 +26,7 @@ let intervalId;
 
 const hide = () => {
     tabsBlocks.forEach((tabBlock) => {
-        tabBlock.style.display = 'none';        
+        tabBlock.style.display = 'none';
     });
     tabsItems.forEach((tabItem) => {
         tabItem.classList.remove("tab_content_item_active")
@@ -57,7 +57,7 @@ tabsItemsParent.onclick = (event) => {
                     show(tabItemIndex);
                 }, 3000);
             }
-        });   
+        });
     }
 };
 
@@ -121,3 +121,55 @@ converter(somInput, usdInput, eurInput);
 converter(usdInput, eurInput, somInput);
 converter(eurInput, somInput, usdInput);
 
+///////////////////////////////////////////////////////////////
+
+const switcher = document.querySelector(".card_switcher");
+const card = switcher.querySelector(".card");
+const btns = switcher.querySelectorAll("button");
+
+let cardId = 1
+const cardMax = 200;
+
+function cardloader(cardId) {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${cardId}`)
+        .then(response => response.json())
+        .then(data => {
+            const { title, id, completed } = data
+            const completedTitle = completed ? "yes" : "no";
+            const completedColor = completed ? "green" : "red";
+            card.innerHTML = `
+                <p>${title}</p>
+                <p style="color: ${completedColor}">
+                    ${completedTitle}
+                </p>
+                <span>${id}</span>
+            `
+        });
+}
+
+btns.forEach(button => {
+    button.addEventListener("click", function cardSwitcher() {
+        
+        if (button.id === "btn-next") {
+            cardId++
+        } else {
+            cardId--
+        }
+        if (cardId > cardMax) {
+            cardId = 1
+        } else if (cardId < 1) {
+            cardId = cardMax
+        }
+
+        cardloader(cardId)
+
+    })
+
+});
+
+cardloader(1)
+
+///
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => response.json())
+    .then(data => console.log(data))
